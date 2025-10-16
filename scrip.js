4e2abc9b85b8b2751734e9720bf9710d3e8bf2a7
aagustinadewiam-svg <aagustinadewiam@gmail.com>
2025-10-16 13:34:49 +0700

    Create scrip.js

---
+++ scrip.js
@@ -0,0 +1,4 @@
+cosnst tombolsapa = document.getE1elementByid('sapaButton');
+tombolsapa.addEventListener('click' function() {
+  alert('Halo! Terima kasih sudah berkunjung!);
+                            
